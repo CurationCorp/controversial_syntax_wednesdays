cd1d44d5910c308e30758270530fa9f1ef3e401a
Henry Dashwood <henry.dashwood@curationcorp.com>
2020-07-15 11:33:09 +0100

    henry's submission

--- 001_vowel_distance_finder/index.js
+++ 001_vowel_distance_finder/index.js
@@ -1,3 +1,28 @@
-module.exports = function findNearestVowels(str) { 
-    return; 
+module.exports = function findNearestVowels(str) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  const vowels = "aeiou".split("");
+  input = str.split("");
+
+  let vowel_indices = [];
+  let input_indices = [];
+  let output = [];
+
+  vowels.forEach((vowel) => vowel_indices.push(alphabet.indexOf(vowel)));
+  input.forEach((char) => input_indices.push(alphabet.indexOf(char)));
+  input_indices.forEach((ind) => output.push(closest(ind, vowel_indices)));
+  return output;
+};
+
+function closest(ind, vowel_indices) {
+  nearest = vowel_indices.reduce((a, b) => {
+    let aDiff = Math.abs(a - ind);
+    let bDiff = Math.abs(b - ind);
+
+    if (aDiff == bDiff) {
+      return a > b ? a : b;
+    } else {
+      return bDiff < aDiff ? b : a;
+    }
+  });
+  return Math.abs(nearest - ind);
 }
