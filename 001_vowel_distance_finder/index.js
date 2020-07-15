1ed1338b2f1e23b2b77b97c2819b1bbeaba33fc1
phat_curationcorp <phat.ma@curationcorp.com>
2020-07-15 10:21:59 +0100

    pass tests

--- 001_vowel_distance_finder/index.js
+++ 001_vowel_distance_finder/index.js
@@ -1,3 +1,18 @@
-module.exports = function findNearestVowels(str) { 
-    return; 
-}
+module.exports = function findNearestVowels(str) {
+  function convertToChar(character) {
+    return character.charCodeAt();
+  }
+  function checkNearestVowel(character) {
+    const vowelsCharCodes = ["a", "e", "i", "o", "u"].map((char) =>
+      convertToChar(char)
+    );
+    return vowelsCharCodes.reduce((prev, curr) => {
+      const distanceFromVowel = Math.abs(character - curr);
+      return distanceFromVowel < prev ? distanceFromVowel : prev;
+    }, character);
+  }
+
+  return [...str]
+    .map((char) => convertToChar(char))
+    .map((charCode) => checkNearestVowel(charCode));
+};
