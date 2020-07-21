154e05f64c8f23ce29448387feef43f25ba3645b
Tom Jennings <tom.jennings@curationcorp.com~>
2020-07-21 18:26:50 +0100

    tests passing....not elegant

---
+++ 002_binary_ones_length/index.js
@@ -0,0 +1,27 @@
+module.exports = function findLargestOneSequence(n) {
+  if (n > 0) {
+    return Number(n)
+      .toString(2)
+      .split("")
+      .map((digit) => parseInt(digit))
+      .reduce(
+        (acc, curr) => {
+          if (curr) {
+            if (acc.run >= acc.highest) {
+              acc.run++;
+              acc.highest = acc.run;
+            } else {
+              acc.run++;
+            }
+          } else {
+            acc.run = 0;
+          }
+          return acc;
+        },
+        { highest: 0, run: 0 }
+      ).highest;
+  }
+  return 0;
+};
+
+// console.log(findLargestOneSequence(-100));
