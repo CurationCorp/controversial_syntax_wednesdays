54ae31479e336f3fb4f66bd23b14d053b76e9bd5
Rob Calcroft <rob.calcroft@curationcorp.com>
2020-07-21 18:20:10 +0100

    robs solution for binary ones

--- 002_binary_ones_length/index.js
+++ 002_binary_ones_length/index.js
@@ -1,3 +1,12 @@
 module.exports = function findLargestOneSequence(n) {
-  return;
+  if (n <= 0) return 0;
+  const binary = n.toString(2);
+  const setOfOnesStrings = binary.split('0').filter(a => a !== '');
+  const result = setOfOnesStrings.reduce((previous, current) => {
+    if (current.length > previous) {
+      return current.length;
+    }
+    return previous;
+  }, 0);
+  return result;
 };
