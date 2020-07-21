35e3ac56fda23dd5899a3c7c187d4a0df26f0e8c
Henry Dashwood <henry.dashwood@curationcorp.com>
2020-07-22 00:15:00 +0100

    henrys submission

--- 002_binary_ones_length/index.js
+++ 002_binary_ones_length/index.js
@@ -1,3 +1,9 @@
 module.exports = function findLargestOneSequence(n) {
-  return;
+  if (n < 1) return 0;
+  return test(n.toString(2), "1".repeat(n.toString(2).length));
+};
+
+const test = (binary, ones) => {
+  if (binary.includes(ones)) return ones.length;
+  return test(binary, "1".repeat(ones.length - 1));
 };
